753aa7a38bc7e31434dd1b722686b75b76b2259e
Bryant_Jason <bryaj185@hsd.k12.or.us>
2019-09-26 13:51:41 -0700

    added the gargoyle game object, and made it a prefab as well. I also made a new script called Outliner

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,32 +11,42 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
             m_IsPlayerAtExit = true;
         }
     }
-    private void Update()
+    void Update()
     {
         if(m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
         }
         
     }
-    void EndLevel ()
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
 
         exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        if(m_Timer > fadeDuration + displayImageDuration)
+        if( doRestart)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
         {
             Application.Quit();
         }
